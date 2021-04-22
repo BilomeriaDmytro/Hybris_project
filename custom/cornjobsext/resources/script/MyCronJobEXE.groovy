3e05c4d8e9abdcb1061f4406d447aa2794ad243d
BilomeriaDmytro <bilomeria.dmytro@gmail.com>
2021-04-22 19:50:28 +0300

    Task 2. Composite cronjob that logs amount of users and orders.

--- custom/cornjobsext/resources/script/MyCronJobEXE.groovy
+++ custom/cornjobsext/resources/script/MyCronJobEXE.groovy
@@ -3,10 +3,29 @@ package script
 cronJobService = spring.getBean("cronJobService");
 fs = spring.getBean("flexibleSearchService");
 
+////// execute cron job that logs amount of orders
+
 query = "select {pk} from {CronJob} where {code} = 'myJobPerformable'"
 result = fs.search(query);
 for(item in result.getResult()){
     myCronJobModel = item;
 }
-
 cronJobService.performCronJob(myCronJobModel,true);
+
+////// execute cron job that logs amount of users
+
+query = "select {pk} from {CronJob} where {code} = 'countOfUsersJobPerformable'"
+result = fs.search(query);
+for(item in result.getResult()){
+    countOfUsersCronJobModel = item;
+}
+cronJobService.performCronJob(countOfUsersCronJobModel,true);
+
+////// execute composite cron job that logs amount of users and orders
+
+query = "select {pk} from {CompositeCronJob} where {code} = 'compositeCronJob'"
+result = fs.search(query);
+for(item in result.getResult()){
+    countOfUsersCronJobModel = item;
+}
+cronJobService.performCronJob(countOfUsersCronJobModel,true);
